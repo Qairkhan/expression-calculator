013926a3e906dcb09b16443f15a7c3de2ebac3d1
qairkhan <shyngys94@gmai.com>
2019-10-12 18:28:38 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -4,9 +4,30 @@ function eval() {
 }
 
 function expressionCalculator(expr) {
-    // write your solution here
+	const bracketOpen = expr.match(/\(/g);
+	const bracketClose = expr.match(/\)/g);
+	let bracketOpenCount;
+	let bracketCloseCount;
+
+	if (bracketOpen) {
+		bracketOpenCount = bracketOpen.length;
+	}
+
+	if (bracketClose) {
+		bracketCloseCount = bracketClose.length;
+	}
+
+	if (bracketOpenCount !== bracketCloseCount) {
+		throw new Error('ExpressionError: Brackets must be paired')
+	} else {
+		let result = (new Function('return ' + expr))();
+		if (result === Infinity) {
+			throw new Error('TypeError: Devision by zero.')
+		}
+		return result
+	}
 }
 
 module.exports = {
-    expressionCalculator
-}
+	expressionCalculator
+};
